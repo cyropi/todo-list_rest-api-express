c93d33b43f3d2337a0a800b58d35548c043d42bf
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-07 19:21:56 +0200

    Update jwtUtils.js

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,1 +1,17 @@
 
+import Jwt from "jsonwebtoken";
+
+
+export function generateToken(username) 
+{
+    return Jwt.sign({ username: username }, 
+                    process.env.TOKEN_SECRET, 
+                    { expiresIn: `${24*60*60}s` });
+}  
+
+
+export function verifyToken(token)
+{
+    return Jwt.verify(token, 
+                      process.env.TOKEN_SECRET);
+}
