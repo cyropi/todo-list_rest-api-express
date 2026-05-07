466fc453f11067d3bb503685c08bf31dce9936c5
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-07 19:24:54 +0200

    Update authenticationRouter.js

--- routes/authenticationRouter.js
+++ routes/authenticationRouter.js
@@ -1,1 +1,35 @@
 
+
+import express from 'express';
+
+import { AuthController } from '../controllers/AuthController.js';
+
+
+export const authenticationRouter = express.Router();
+
+
+authenticationRouter.route('/auth')
+                    .post(async (req, res) => { 
+                                                  const token = await AuthController.checkCredentials(req, res); 
+
+                                                  if (token)
+                                                    res.json({ token: token });
+                                                  else
+                                                  {
+                                                      res.status(401);
+                                                      res.json({ errors: ["Invalid credentials. Try again."] });
+                                                  }
+                                              });
+
+
+authenticationRouter.route('/signup')
+                    .post(async (req, res) => { 
+                                                  AuthController.saveUser(req, res).then((user) => {
+                                                                                                       res.status(201);
+                                                                                                       res.json(user);
+                                                                                                   })
+                                                                                   .catch((err) => {
+                                                                                                       res.status(500);
+                                                                                                       res.json({ errors: [`Could not save user (${err}). Try again.`] });
+                                                                                                   });         
+                                              });
