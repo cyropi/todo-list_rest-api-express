85b051b64e7cd16ef45e107c39f2377f041a15ca
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-11 23:43:05 +0200

    Update authentication.js

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,1 +1,27 @@
 
+import { verifyToken } from '../utils/jwtUtils.js';
+
+
+export function enforceAuthentication(req, res, next)
+{
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(' ')[1];
+
+    if (!token)
+    {
+        next({ status: 401, message: "Unauthorized" });
+        return;
+    }
+
+    try 
+    {
+        const decodedToken = verifyToken(token);
+        req.username = decodedToken.username;
+
+        next();
+    }
+    catch (error)
+    {
+        next({ status: 401, message: "Unauthorized" });
+    }
+}
