a730cb596e0cbfa31a414c9a39a58c1a7754d572
Ciro Pizza <ciropizza2002@gmail.com>
2026-05-12 21:32:18 +0200

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,1 +1,21 @@
 
+import { TodoController } from '../controllers/TodoController.js';
+
+
+export async function ensureUsersModifyOnlyOwnTodos(req, res, next)
+{
+	const user = req.username;
+	const todoId = req.params.id;
+	
+	const userHasPermission = await TodoController.canUserModifyTodo(user, todoId);
+
+  	if (userHasPermission)
+		next();
+ 	else 
+	{
+		next({ 
+			     status: 403, 
+			     message: "Forbidden! You do not have permissions to view or modify this resource." 
+			 });
+  	}
+}
